e256d59a3827fbefa22d4f858bd496067ab87f36
khushnarola12 <khushnarola12@gmail.com>
2025-05-02 10:50:35 +0530

    add all origin to access

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
